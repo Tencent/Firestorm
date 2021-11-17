69ee8a810ae9291478609330c4e4e1df872ff248
roryqi <roryqi@tencent.com>
2021-11-17 15:36:11 +0800

    Fix

--- server/src/main/java/com/tencent/rss/server/HealthCheck.java
+++ server/src/main/java/com/tencent/rss/server/HealthCheck.java
@@ -25,7 +25,6 @@ import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.File;
 import java.lang.reflect.Constructor;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
