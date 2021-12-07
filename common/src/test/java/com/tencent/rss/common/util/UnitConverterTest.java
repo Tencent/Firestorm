3769c4cb9c480d29739932640ccdb4abb65d2ab7
frankliee <frankzfli@tencent.com>
2021-12-07 21:00:20 +0800

    comment fix

--- common/src/test/java/com/tencent/rss/common/util/UnitConverterTest.java
+++ common/src/test/java/com/tencent/rss/common/util/UnitConverterTest.java
@@ -36,10 +36,14 @@ public class UnitConverterTest {
     assertEquals(10 * PB, UnitConverter.byteStringAs("10PB", ByteUnit.BYTE));
     assertEquals(10 * PB, UnitConverter.byteStringAs("10pb", ByteUnit.BYTE));
     assertEquals(10 * PB, UnitConverter.byteStringAs("10pB", ByteUnit.BYTE));
+    assertEquals(10 * PB, UnitConverter.byteStringAs("10p", ByteUnit.BYTE));
+    assertEquals(10 * PB, UnitConverter.byteStringAs("10P", ByteUnit.BYTE));
 
     assertEquals(10 * TB, UnitConverter.byteStringAs("10TB", ByteUnit.BYTE));
     assertEquals(10 * TB, UnitConverter.byteStringAs("10tb", ByteUnit.BYTE));
     assertEquals(10 * TB, UnitConverter.byteStringAs("10tB", ByteUnit.BYTE));
+    assertEquals(10 * TB, UnitConverter.byteStringAs("10T", ByteUnit.BYTE));
+    assertEquals(10 * TB, UnitConverter.byteStringAs("10t", ByteUnit.BYTE));
 
     assertEquals(10 * GB, UnitConverter.byteStringAs("10GB", ByteUnit.BYTE));
     assertEquals(10 * GB, UnitConverter.byteStringAs("10gb", ByteUnit.BYTE));
@@ -48,10 +52,14 @@ public class UnitConverterTest {
     assertEquals(10 * MB, UnitConverter.byteStringAs("10MB", ByteUnit.BYTE));
     assertEquals(10 * MB, UnitConverter.byteStringAs("10mb", ByteUnit.BYTE));
     assertEquals(10 * MB, UnitConverter.byteStringAs("10mB", ByteUnit.BYTE));
+    assertEquals(10 * MB, UnitConverter.byteStringAs("10M", ByteUnit.BYTE));
+    assertEquals(10 * MB, UnitConverter.byteStringAs("10m", ByteUnit.BYTE));
 
     assertEquals(10 * KB, UnitConverter.byteStringAs("10KB", ByteUnit.BYTE));
     assertEquals(10 * KB, UnitConverter.byteStringAs("10kb", ByteUnit.BYTE));
     assertEquals(10 * KB, UnitConverter.byteStringAs("10Kb", ByteUnit.BYTE));
+    assertEquals(10 * KB, UnitConverter.byteStringAs("10K", ByteUnit.BYTE));
+    assertEquals(10 * KB, UnitConverter.byteStringAs("10k", ByteUnit.BYTE));
 
     assertEquals(1111, UnitConverter.byteStringAs("1111", ByteUnit.BYTE));
   }
